78dc4c99cc8990c5bfa5f91e70996e8666523240
Piotr Wawrzyniak <piotr@wawrzyniak.poznan.pl>
2023-04-23 18:17:02 +0200

    Add missing setup.py and add reference to numpy

--- spm.py
+++ spm.py
@@ -1,5 +1,10 @@
+import numpy
+
 def add(a, b):
-    return a + b
+    arrA = np.array([a])
+    arrB = np.array([b])
+    arrC numpy.add(arrA, arrB)
+    return arrC[0]
 
 def subtract(a, b):
     return a - b
