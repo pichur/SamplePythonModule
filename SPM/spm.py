4358be029f3bbb5dd04b80bbd7d191782f9b9bdb
Piotr Wawrzyniak <piotr@wawrzyniak.poznan.pl>
2023-04-23 21:13:32 +0200

    Revert "Move to folder"
    This reverts commit 4b388889a27aff915f8c5cc9798929b150caab7c.

--- SPM/spm.py
+++
@@ -1,7 +0,0 @@
-from termcolor import colored
-
-def add(a, b):
-    print(colored(str(a), "blue") + " + " + colored(str(b), "blue") + " = " + colored(str(a+b), "green"))
-
-def subtract(a, b):
-    print(colored(str(a), "blue") + " - " + colored(str(b), "blue") + " = " + colored(str(a-b), "red"))
